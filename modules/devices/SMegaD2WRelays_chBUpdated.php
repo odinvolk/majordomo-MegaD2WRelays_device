f96f00f039c22a0dde0817a7824009f4a2fd04b2
Паша <odinwolk@gmail.com>
2020-05-25 14:15:20 +0300

    Create SMegaD2WRelays_chBUpdated.php

---
+++ modules/devices/SMegaD2WRelays_chBUpdated.php
@@ -0,0 +1,2 @@
+<?php
+getURL("http://".$this->getProperty('ipAddress')."/".$this->getProperty('Password')."/?cmd=".$this->getProperty('Port')."B:".$this->getProperty('chB')."&addr=".$this->getProperty('bAddress'));
